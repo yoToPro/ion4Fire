15fdbed98a6f766b392a224dad819cda101b8816
Dhirendra Kumar <dhirendra@srinidhis-mini.indience.in>
2019-02-19 19:01:29 +0530

    Initial Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDdflzK1tniUt50KiOHBd0imvuUlEGLbdE",
+    authDomain: "ion4fire-a4553.firebaseapp.com",
+    databaseURL: "https://ion4fire-a4553.firebaseio.com",
+    projectId: "ion4fire-a4553",
+    storageBucket: "ion4fire-a4553.appspot.com",
+    messagingSenderId: "108634626318"
+  }
 };
 
 /*
